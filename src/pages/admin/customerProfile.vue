c65e6572b06d621bb66c7e9f7c032ae13c0d27b3
Sivakavi Jayadevan <sivakavi63@gmail.com>
2018-10-02 12:04:39 +0530

    [new] customer - subscription assign work

--- src/pages/admin/customerProfile.vue
+++ src/pages/admin/customerProfile.vue
@@ -199,6 +199,84 @@
       </q-card>
     </q-modal>
 
+    <q-modal no-esc-dismiss no-backdrop-dismiss class="smart-model-view" v-model="subscriptionAdd" :content-css="{ minWidth: '50vw'}">
+      <q-card>
+        <q-card-title class="model-header">
+          <div class="model-title">Customer - Subscription Add</div>
+          <q-btn @click="subscriptionAdd = false" flat icon="clear" class="header-btn" color="white" slot="right"></q-btn>
+        </q-card-title>
+        <q-card-main class="model-main">
+            <div class="form-body">
+              <div class="form-group">
+                <div class="row gutter-sm">
+                  <div class="col-md-4">
+                    <label class="control-label">Subscription</label>
+                  </div>
+                <div class="col-md-6">
+                  <q-select class="form-input" @input="selectedSubscriptionOption" v-model="selectedSubscription" :options="subscriptionOptions"/>
+                  <span class="form-group__error" v-if="$v.selectedSubscription.$error && !$v.selectedSubscription.required">Field is required.</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="row gutter-sm">
+                <div class="col-md-4">
+                  <label class="control-label">Amount</label>
+                </div>
+                <div class="col-md-6">
+                  <q-input class="form-input" v-model="subscriptionAmount"/>
+                  <span class="form-group__error" v-if="$v.subscriptionAmount.$error && !$v.subscriptionAmount.required">Field is required.</span>
+                  <span class="form-group__error" v-if="$v.subscriptionAmount.$error && !$v.subscriptionAmount.numericWithDot">Accept numeric value only.</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="row gutter-sm">
+                <div class="col-md-4">
+                  <label class="control-label">Duration (Months)</label>
+                </div>
+                <div class="col-md-6">
+                  <q-input readonly class="form-input" v-model="subscriptionDuration"/>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="row gutter-sm">
+                <div class="col-md-4">
+                  <label class="control-label">Subscriptoin Start Date</label>
+                </div>
+                <div class="col-md-6">
+                  <q-datetime @change="startDateChange" class="form-input" format="DD-MM-YYYY" inverted color="light-blue" :first-day-of-week="1" v-model="subscriptionStart"/>
+                  <span class="form-group__error" v-if="$v.subscriptionStart.$error && !$v.subscriptionStart.required">Field is required.</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="row gutter-sm">
+                <div class="col-md-4">
+                  <label class="control-label">Subscriptoin Start End</label>
+                </div>
+                <div class="col-md-6">
+                  <q-input readonly class="form-input" v-model="subscriptionEnd"/>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        
+        </q-card-main>
+       <q-card-actions class="model-footer border-1px">
+          <div style="width: 100%">
+              <q-btn @click="assignSubscription" class="add-btn float-right">Assign Subscription</q-btn>
+          </div>
+        </q-card-actions>
+      </q-card>
+    </q-modal>
+
   </q-page>
 </template>
 
@@ -206,6 +284,25 @@
 
 import api from 'src/services/api/api'
 import moment from 'moment';
+import { 
+    required, 
+    email,
+    minLength,
+    maxLength,
+    numeric,
+    alpha
+} from 'vuelidate/lib/validators'
+
+import {
+  alphaWithSpaceDot,
+  alphaWithSpace,
+  alphaWithDot,
+  alphaNumericWithSpaceDot,
+  alphaNumericWithSpace,
+  alphaNumericWithDot,
+  alphaNumericWithSpaceHyphen,
+  numericWithDot
+} from 'src/services/shared/customValidation.js'
 
 export default {
   name: 'CustomerProfile',
@@ -285,12 +382,34 @@ export default {
         rowsPerPage: 10
       },
       subscriptionView: false,
-      singleSubscription: {}
+      singleSubscription: {},
+      subscriptionAdd: false,
+      selectedSubscription: '',
+      subscriptionList: [],
+      subscriptionOptions: [],
+      subscriptionAmount: '',
+      subscriptionDuration: '',
+      subscriptionStart: '',
+      subscriptionEnd: ''
     }
   },
 
   computed: {
     
+  },
+
+  validations(){
+      return {
+            selectedSubscription:{
+              required
+            },
+            subscriptionAmount:{
+              required
+            },
+            subscriptionStart:{
+              required
+            },
+      }
   },
 
   methods: {
@@ -300,7 +419,15 @@ export default {
     },
 
     addSubscription(){
-      this.$router.push('/admin/addgsttype')
+      this.selectedSubscription = ''
+      this.subscriptionAmount = ''
+      this.subscriptionDuration = ''
+      this.subscriptionStart = ''
+      this.subscriptionEnd = ''
+      this.$v.selectedSubscription.$reset()
+      this.$v.subscriptionAmount.$reset()
+      this.$v.subscriptionStart.$reset()
+      this.subscriptionAdd = true
     },
 
     getCustomer(){
@@ -319,6 +446,95 @@ export default {
     viewSubscription(subscription){
       this.singleSubscription = subscription
       this.subscriptionView = true
+    },
+
+    getSubscriptionList(){
+      let self = this;
+      api
+        .get('subscriptions')
+        .then(function(response) {
+          self.subscriptionList = response.data.data;
+          let filterArray = self.subscriptionList.filter(function(item){
+            return item.is_active == '1';
+          })
+          filterArray.forEach(function(element) {
+            let singleItem = {
+              label: element.name + ' - ( ' + element.duration + ' ) - ( ' + element.amount + ' )',
+              value: element
+            }
+            self.subscriptionOptions.push(singleItem)
+          });
+        })
+        .catch(function(error) {
+          console.log("subscription table get data error---",error);
+        });
+    },
+
+    selectedSubscriptionOption(){
+      this.subscriptionAmount = this.selectedSubscription.amount
+      this.subscriptionDuration = this.selectedSubscription.duration
+    },
+
+    startDateChange(){
+      if(this.subscriptionDuration){
+        let addMonths = parseInt(this.subscriptionDuration)
+        let endDate = moment(this.subscriptionStart).add(addMonths, 'M')
+        let endDateFormat = moment(endDate).format('DD-MM-YYYY')
+        this.subscriptionEnd = endDateFormat
+      }else{
+        this.subscriptionStart = ''
+        this.$q.notify({
+            position: "top",
+            timeout: 2000,
+            message: 'Please select Subscription..'
+        })
+      }
+    },
+
+    assignSubscription(){
+      this.$v.selectedSubscription.$touch()
+      this.$v.subscriptionAmount.$touch()
+      this.$v.subscriptionStart.$touch()
+      if (this.$v.selectedSubscription.$error || this.$v.subscriptionAmount.$error || this.$v.subscriptionStart.$error) {
+        this.$q.notify({
+          position: "top",
+          message: "Please review fields again."
+        })
+        return
+      }
+      
+      let self = this
+      let url = "customersubscriptions"
+      let requestData = {
+        subscription_id: ""+this.selectedSubscription.id,
+        customer_id: this.customerID,
+        amount: this.subscriptionAmount,
+        doj: moment(this.subscriptionStart).format('YYYY-MM-DD')
+      }
+
+      //console.log(requestData)
+      
+      api
+        .post(url, requestData)
+        .then(function (response) {
+          self.$q.notify({
+              position: "top",
+              type: 'positive',
+              timeout: 2000,
+              message: response.data.message
+          })
+          self.getCustomer()
+          self.subscriptionAdd = false
+        })
+        .catch(function (error) {
+          if(!error.response.data.success){
+            self.$q.notify({
+              position: "top",
+              timeout: 2000,
+              message: error.response.data.message
+            })
+          }
+        });
     }
     
   },
@@ -350,6 +566,7 @@ export default {
   created(){
     this.customerID = this.$route.params.id;
     this.getCustomer()
+    this.getSubscriptionList()
   }
   
 }
